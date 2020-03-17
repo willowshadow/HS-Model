4fea31b785dbbaa7f05b89cf143a8290daa001ae
willowshadow <danialtariq43@gmail.com>
2020-03-17 13:25:06 +0500

    Add project files.

---
+++ Loader.py
@@ -0,0 +1,40 @@
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+from nltk.corpus import stopwords
+#import HS_Model
+import re
+
+
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+def clean_text(text):
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+    text = text.lower() # lowercase text
+    text = REPLACE_BY_SPACE_RE.sub(' ', text) # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
+    text = BAD_SYMBOLS_RE.sub('', text) # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing. 
+    text = text.replace('x', '')
+#    text = re.sub(r'\W+', '', text)
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove stopwors from text
+    return text
+
+new_complaint = 'nice dude'
+
+newnew_complaint= clean_text(new_complaint)
+
+tokenizer = Tokenizer(num_words=5000, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+model = load_model('Model.h5',compile=False)
+#model.summary()
+
+seq = tokenizer.texts_to_sequences(new_complaint)
+padded = pad_sequences(seq, maxlen=280)
+pred = model.predict_proba(padded)
+labels = ['Not Hate','Hate']
+print(pred, labels[np.argmax(pred)])
